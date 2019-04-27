86f78e7c05148216b4856f957c3918bba111ccb2
sandunsameera <sandunsameera25@gmail.com>
2019-04-28 00:27:31 +0530

    Innitial commit

---
+++ untitled/lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Flutter Demo',
+      color: Colors.black,
+      home: new MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+
+  @override
+  _MyHomePageState createState() => new _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Flutter with firebase"),
+        backgroundColor: Colors.black,
+      ),
+    );
+
+  }
+}
